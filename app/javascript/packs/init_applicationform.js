45468b8784f1adc2e89d04ea537fbdde9533c2f3
Amir Mourali <amourali7135@gmail.com>
2022-03-12 01:12:49 +0800

    More form work

---
+++ app/javascript/packs/init_applicationform.js
@@ -0,0 +1,18 @@
+// const initApplicationform = () => {
+//     var restaurantCheckbox = document.getElementById('business_restaurant');
+//     if (restaurantCheckbox) {
+//         restaurantCheckbox.addEventListener('change', function() {
+//             if (restaurantCheckbox.checked) {
+//                 document.getElementById("restaurant_details").style.display = "block"; }
+//                 else {
+//                     document.getElementById("restaurant_details").style.display = "none"; }
+//                 });
+//             }   
+//             // console.log("this shit again")
+//         }     
+        
+//         export { initRestaurantform };
+        
+//         // window.addEventListener('DOMContentLoaded', (event) => {
+//         //     // My Code
+//         // });
--- app/models/application.rb
+++ app/models/application.rb
@@ -4,8 +4,19 @@ class Application < ApplicationRecord
 
   enum :status, { pending: 0, interested: 1, rejected: 2, accepted: 3 }, default: :pending
 
-  validates :first_essay, length: {minimum: 50, maximum: 1500}, presence: true, if: @program.essay_question_one.present?
-  validates :second_essay, length: {minimum: 50, maximum: 1500}, presence: true, if: @program.essay_question_two.present?
-  validates :third_essay, length: {minimum: 50, maximum: 1500}, presence: true, if: @program.essay_question_three.present?
+  validates :first_essay, length: {minimum: 50, maximum: 1500}, presence: true, if: :essay_one?
+  validates :second_essay, length: {minimum: 50, maximum: 1500}, presence: true, if: :essay_two?
+  validates :third_essay, length: {minimum: 50, maximum: 1500}, presence: true, if: :essay_three
 
+  def essay_one?
+    return true if params[:essay_one_needed] == true
+  end
+
+  def essay_two?
+    return true if params[:essay_two_needed] == true
+  end
+
+  def essay_three?
+    return true if params[:essay_three_needed] == true
+  end
 end
