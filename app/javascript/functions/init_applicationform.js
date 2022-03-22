25d5991ceabce307375f416153b27fd6727ddc20
Amir Mourali <amourali7135@gmail.com>
2022-03-22 22:19:52 +0800

    Application form js work-around done

---
+++ app/javascript/functions/init_applicationform.js
@@ -0,0 +1,10 @@
+// const initApplicationform = () => {
+//   var applicationOne = document.getElementById("application_essay_one");
+//   var applicationTwo = document.getElementById("application_essay_two");
+//   var applicationThree = document.getElementById("application_essay_three");
+
+
+
+// };
+
+// export { initApplicationform };
